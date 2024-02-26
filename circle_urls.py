1c8a460991bc256077fd2d970e79e5d0cf6d73ef
Chris Karr <chris@audacious-software.com>
2024-02-26 17:14:32 -0600

    Update circle_urls.py

--- circle_urls.py
+++ circle_urls.py
@@ -5,14 +5,14 @@ import sys
 import django
 
 if sys.version_info[0] > 2:
-    from django.urls import re_path
+    from django.urls import include, re_path
 
     urlpatterns = [
         re_path(r'^admin/', django.contrib.admin.site.urls),
         re_path(r'^messages/', include('simple_messaging.urls')),
     ]
 else:
-    from django.conf.urls import url
+    from django.conf.urls import include, url
 
     urlpatterns = [
         url(r'^admin/', django.contrib.admin.site.urls),
